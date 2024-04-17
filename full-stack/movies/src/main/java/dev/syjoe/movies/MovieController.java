aa33dfb9f706b2a8cc02a3d5e59a87e211067640
syjoe <csy5501@gmail.com>
2024-04-17 09:55:56 +0900

    url test

---
+++ full-stack/movies/src/main/java/dev/syjoe/movies/MovieController.java
@@ -0,0 +1,14 @@
+package dev.syjoe.movies;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public String allMovies() {
+        return "All movies";
+    }
+}
